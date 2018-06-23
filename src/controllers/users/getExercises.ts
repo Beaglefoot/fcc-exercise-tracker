01d424a0c60888e2103c6ed389af33025635a248
Beaglefoot <ch-e-st@yandex.ru>
2018-06-24 00:22:47 +0300

    Handle limit query parameter

--- src/controllers/users/getExercises.ts
+++ src/controllers/users/getExercises.ts
@@ -5,19 +5,26 @@ import isValidDate from '../../helpers/validateDate';
 
 const getExercises: Handler = async (req, res, next) => {
   const { userId } = req.params;
-  const { from, to } = req.query;
+  const { from, to, limit } = req.query;
   let user: IUser;
 
   Object.entries({ from, to }).map(([key, date]) => {
     if (date && !isValidDate(date)) {
       throw Object.assign(
         new Error(
-          `"${key}" query parameter should have a valid format of yyyy-mm-dd`
+          `"${key}" query parameter should have a valid format of yyyy-mm-dd.`
         ),
         { httpStatusCode: 400 }
       );
     }
   });
+
+  if (limit !== undefined && !limit.match(/[0-9]/)) {
+    throw Object.assign(
+      new Error('"limit" query parameter should be a number.'),
+      { httpStatusCode: 400 }
+    );
+  }
 
   try {
     user = await User.aggregate([
@@ -38,7 +45,19 @@ const getExercises: Handler = async (req, res, next) => {
             }
           }
         }
-      }
+      },
+      ...(limit !== undefined
+        ? [
+            {
+              $project: {
+                name: true,
+                exercises: {
+                  $slice: ['$exercises', 0, parseInt(limit, 10)]
+                }
+              }
+            }
+          ]
+        : [])
     ]).then(([user]) => user);
   } catch (err) {
     throw Object.assign(
